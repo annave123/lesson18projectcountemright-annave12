a235f2b281d6b4600e3e0af8dede4a44c0b2aa5d
annave123 <tony.nave@aol.com>
2016-10-25 22:26:06 -1000

    hello

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,14 +5,14 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
-        int ch[] = {66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,89,90};
+        double ch[] = {66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,89,90};
 
-        ch.replace (ch[0] = 'A', ch[1] = 'B', ch[2]= 'C', ch[3] = 'D', ch[4] = 'E', ch[5]= 'F', ch[6] = 'G', ch[7] = 'H', ch[8]= 'I', ch[9] = 'J', ch[10] = 'K', ch[11]= 'L'
+        ch = (ch[0] = 'A', ch[1] = 'B', ch[2]= 'C', ch[3] = 'D', ch[4] = 'E', ch[5]= 'F', ch[6] = 'G', ch[7] = 'H', ch[8]= 'I', ch[9] = 'J', ch[10] = 'K', ch[11]= 'L'
         ch[12] = 'M', ch[13] = 'N', ch[14]= 'O', ch[15] = 'P', ch[16] = 'Q', ch[17]= 'R', ch[18] = 'S', ch[19] = 'T', ch[20]= 'U', ch[20] = 'V', ch[21] = 'W', ch[22]= 'X'
         ch[23] = 'X', ch[24] = 'Y', ch[25]= 'Z');
 
 
-        for(ch= 1; ch < 91; ch++){
+        for(ch= 1.0; ch < 91; ch++){
             System.out.println(ch);
 }
 
